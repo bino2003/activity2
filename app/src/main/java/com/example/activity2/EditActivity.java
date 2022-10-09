6e51b528f10e8fea8641da44b345cebda1d70af7
MuathNassar <m.nassar@unitone.co>
2022-10-09 19:55:20 +0300

    Muath made some fixes

--- app/src/main/java/com/example/activity2/EditActivity.java
+++ app/src/main/java/com/example/activity2/EditActivity.java
@@ -14,36 +14,77 @@ import android.widget.EditText;
 import com.example.activity2.databinding.ActivityEditActiviyBinding;
 
 public class EditActivity extends AppCompatActivity {
-ActivityEditActiviyBinding binding;
+    private String courseName;
+    private int courseSemester;
+    private int courseHours;
+    private int position;
+    ActivityEditActiviyBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityEditActiviyBinding.inflate(getLayoutInflater());
-
+        binding = ActivityEditActiviyBinding.inflate(getLayoutInflater());
+        getData();
+        renderData();
         setContentView(binding.getRoot());
-        ActivityResultLauncher<Intent>   arl=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        /*
+             ActivityResultLauncher<Intent> arl = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
-                binding.etCourseHours.setText(result.getData().getIntExtra("CourseHours",0));
+                binding.etCourseHours.setText(result.getData().getIntExtra("CourseHours", 0));
                 binding.etCourseName.setText(result.getData().getStringExtra("CourseName"));
-                binding.etCourseSemester.setText(result.getData().getIntExtra("Course_semester",0));
+                binding.etCourseSemester.setText(result.getData().getIntExtra("Course_semester", 0));
 
             }
         });
+         */
+
 
         binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(EditActivity.this,Item_Course.class);
-                arl.launch(intent);
+                Intent intent = new Intent(EditActivity.this, Item_Course.class);
 
-
-
+            }
+        });
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int  Coursehours=  Integer.parseInt(String.valueOf(binding.etCourseHours.getText()));
+                String Coursename=binding.etCourseName.getText().toString();
+                int Coursesemester=Integer.parseInt(binding.etCourseSemester.getText().toString());
+                Intent intent =new Intent(EditActivity.this,MainActivity.class);
+                intent.putExtra("Coursehours",Coursehours);
+                intent.putExtra("Coursename",Coursename);
+                intent.putExtra("Coursesemester",Coursesemester);
+                intent.putExtra("position",position);
+                startActivity(intent);
+                finishAffinity();
             }
         });
 
 
+    }
 
+    /*
+            intent.putExtra("Course_semester",CourseSemester);
+            intent.putExtra("CourseHours",CourseHours);
+            intent.putExtra("CourseName",CourseName);
+     */
+    private void getData() {
+        if (this.getIntent() != null) {
+            Intent i = this.getIntent();
+            courseName = i.getStringExtra("CourseName");
+            courseHours = i.getIntExtra("CourseHours", 0);
+            courseSemester = i.getIntExtra("Course_semester", 0);
+            position = i.getIntExtra("position", 0);
+
+        }
+    }
+    private void renderData(){
+        binding.etCourseHours.setText(courseHours+"");
+        binding.etCourseName.setText(courseName);
+        binding.etCourseSemester.setText(courseSemester+"");
 
     }
 }
--- app/src/main/java/com/example/activity2/MainActivity.java
+++ app/src/main/java/com/example/activity2/MainActivity.java
@@ -12,27 +12,24 @@ import com.example.activity2.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements OnAction {
-ActivityMainBinding binding;
+    ActivityMainBinding binding;
     Course_adapter course_adapter;
-ArrayList<course> courses=new ArrayList<>();
+    ArrayList<course> courses = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-      courses.add(new course("UiUx",3,1));
-        courses.add(new course("Web Programming",3,1));
-        courses.add(new course("English IT",3,1));
-        course_adapter=new Course_adapter(courses,this);
-
+        courses.add(new course("UiUx", 3, 1));
+        courses.add(new course("Web Programming", 3, 1));
+        courses.add(new course("English IT", 3, 1));
+        course_adapter = new Course_adapter(courses, this);
 
 
         binding.rvCourse.setAdapter(course_adapter);
-        binding.rvCourse.setLayoutManager(new LinearLayoutManager(MainActivity.this,RecyclerView.VERTICAL,false) );
-
-
-
+        binding.rvCourse.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false));
 
 
     }
@@ -40,26 +37,33 @@ ArrayList<course> courses=new ArrayList<>();
     @Override
     public void onDelete(int position) {
         courses.remove(position);
-       course_adapter. notifyItemRemoved(position);
+        course_adapter.notifyItemRemoved(position);
     }
 
     @Override
     public void onEdit(int Position) {
 
 
-        int CourseHours =courses.get(Position).getCourse_hours();
-        int CourseSemester =courses.get(Position).getCourse_semester();
-        String CourseName =courses.get(Position).getCourse_name();
+        int CourseHours = courses.get(Position).getCourse_hours();
+        int CourseSemester = courses.get(Position).getCourse_semester();
+        String CourseName = courses.get(Position).getCourse_name();
 
 
-        Intent intent=new Intent( );
-        intent.putExtra("Course_semester",CourseSemester);
-        intent.putExtra("CourseHours",CourseHours);
-        intent.putExtra("CourseName",CourseName);
-        setResult(RESULT_OK,intent);
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra("Course_semester", CourseSemester);
+        intent.putExtra("CourseHours", CourseHours);
+        intent.putExtra("CourseName", CourseName);
+        intent.putExtra("position", Position);
+        //setResult(RESULT_OK,intent);
+        MainActivity.this.startActivity(intent);
         finish();
 
 
+    }
+    private void updateAtGivenPosition(int position){
+        course c = courses.get(position);
+        //getIntent().getStringExtra()
+        //c.setCourse_hours();
     }
 
 
